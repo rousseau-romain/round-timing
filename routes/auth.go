a6cc35dac3b3e4b36923939ddee8db97c824ece3
Romain Rousseau <rousseau.romain82@gmail.com>
2026-01-27 14:43:29 +0100

    add rate limit

--- routes/auth.go
+++ routes/auth.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/gorilla/mux"
 	handlersAuth "github.com/rousseau-romain/round-timing/handlers/auth"
@@ -9,12 +10,14 @@ import (
 	"github.com/rousseau-romain/round-timing/service/auth"
 )
 
+var authLimiter = middleware.NewRateLimiter(10, 15*time.Minute)
+
 func registerAuthRoutes(r *mux.Router, handler *handlersAuth.Handler, authService *auth.AuthService, logger *slog.Logger) {
 	r.Handle("/signup", middleware.RequireNotAuth(handler.HandleSignupEmail, authService, logger)).Methods("GET")
 	r.Handle("/signin", middleware.RequireNotAuth(handler.HandleLogin, authService, logger)).Methods("GET")
 
-	r.HandleFunc("/signup", handler.HandleCreateUser).Methods("POST")
-	r.HandleFunc("/signin", handler.HandleLoginEmail).Methods("POST")
+	r.HandleFunc("/signup", authLimiter.Limit(handler.HandleCreateUser)).Methods("POST")
+	r.HandleFunc("/signin", authLimiter.Limit(handler.HandleLoginEmail)).Methods("POST")
 	r.HandleFunc("/auth/{provider}", handler.HandleProviderLogin).Methods("GET")
 	r.HandleFunc("/auth/{provider}/callback", handler.HandleAuthCallbackFunction).Methods("GET")
 	r.HandleFunc("/auth/logout/{provider}", handler.HandleLogout).Methods("GET")
