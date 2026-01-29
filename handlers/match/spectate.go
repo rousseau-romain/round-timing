c7bec6f1f6f39d5fc6aa298fd4f45a0209c7ae37
rousseau-romain <47084618+rousseau-romain@users.noreply.github.com>
2026-01-29 16:48:45 +0000

    feat: add real time spectate (#57)

--- handlers/match/spectate.go
+++ handlers/match/spectate.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/a-h/templ"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"github.com/rousseau-romain/round-timing/config"
-	"github.com/rousseau-romain/round-timing/service/auth"
 	matchModel "github.com/rousseau-romain/round-timing/model/match"
 	userModel "github.com/rousseau-romain/round-timing/model/user"
+	"github.com/rousseau-romain/round-timing/pkg/notify"
+	"github.com/rousseau-romain/round-timing/service/auth"
 	pageMatch "github.com/rousseau-romain/round-timing/views/page/match"
 )
 
@@ -25,8 +25,6 @@ var upgrader = websocket.Upgrader{
 		return allowedOrigins[origin]
 	},
 }
-
-var clients = make(map[string]*websocket.Conn) // Maps userID to WebSocket connection
 
 func (h *Handler) HandleSpectateMatch(w http.ResponseWriter, r *http.Request) {
 	user, _ := auth.UserFromRequest(r)
@@ -79,57 +77,61 @@ func (h *Handler) HandleMatchTableLive(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 	matchId, _ := strconv.Atoi(vars["idMatch"])
-	userMatchUniqueString := fmt.Sprintf("%d-%d", user.Id, matchId)
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Error upgrading to WebSocket:", err)
 		return
 	}
-	conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	defer conn.Close()
 
-	clients[userMatchUniqueString] = conn
+	ch := notify.Subscribe(matchId)
+	defer notify.Unsubscribe(matchId, ch)
 
-	defer func() {
-		delete(clients, userMatchUniqueString)
-		conn.Close()
+	// Detect client disconnect via ReadMessage.
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for {
+			if _, _, err := conn.ReadMessage(); err != nil {
+				return
+			}
+		}
 	}()
 
 	for {
-		time.Sleep(2 * time.Second)
-
-		match, err := matchModel.GetMatch(matchId)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		select {
+		case <-done:
 			return
-		}
-
-		players, err := matchModel.GetPlayersByIdMatch(user.IdLanguage, matchId)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		userConfigurationFavoriteSpells, err := userModel.GetConfigurationByIdConfigurationIdUser(user.IdLanguage, user.Id, 1)
-		if err != nil {
-			h.Slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		spellsPlayers, err := matchModel.GetSpellsPlayersByIdMatch(user.IdLanguage, matchId, user.Id, userConfigurationFavoriteSpells.IsEnabled)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		message, _ := templ.ToGoHTML(r.Context(), pageMatch.PlayerTable(match, players, spellsPlayers, true))
-		for client := range clients {
-			c := clients[userMatchUniqueString]
-			err := c.WriteMessage(websocket.TextMessage, []byte(message))
+		case <-ch:
+			match, err := matchModel.GetMatch(matchId)
 			if err != nil {
-				fmt.Println("Error writing to WebSocket:", err)
-				c.Close()
-				delete(clients, client)
+				h.Slog.Error(err.Error())
+				return
+			}
+
+			players, err := matchModel.GetPlayersByIdMatch(user.IdLanguage, matchId)
+			if err != nil {
+				h.Slog.Error(err.Error())
+				return
+			}
+
+			userConfigurationFavoriteSpells, err := userModel.GetConfigurationByIdConfigurationIdUser(user.IdLanguage, user.Id, 1)
+			if err != nil {
+				h.Slog.Error(err.Error())
+				return
+			}
+
+			spellsPlayers, err := matchModel.GetSpellsPlayersByIdMatch(user.IdLanguage, matchId, user.Id, userConfigurationFavoriteSpells.IsEnabled)
+			if err != nil {
+				h.Slog.Error(err.Error())
+				return
+			}
+
+			message, _ := templ.ToGoHTML(r.Context(), pageMatch.PlayerTable(match, players, spellsPlayers, true))
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+				h.Slog.Error("Error writing to WebSocket: " + err.Error())
+				return
 			}
 		}
 	}
