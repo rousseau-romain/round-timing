9f8069626fc428d0c8ab000d5dbdeb74bc5fe0a0
rousseau-romain <47084618+rousseau-romain@users.noreply.github.com>
2026-02-04 09:17:48 +0000

    Feat: rework UI (#58)
    * updateProfile
    
    * rework ui copy button
    
    * add config expanded
    
    * add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
   safelist: [
     // Team background colors
     { pattern: /bg-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(50|100|200|300|400|500)/ },
+    // Team background colors (dark mode)
+    { pattern: /bg-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(800|900)/, variants: ['dark'] },
     // Team text colors
     { pattern: /text-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(500|600|700|800)/ },
     // Team border colors
-    { pattern: /border-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(300|400|500)/ },
+    { pattern: /border-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(200|300|400|500)/ },
+    // Team border colors (dark mode)
+    { pattern: /border-(red|blue|green|yellow|purple|pink|orange|cyan|indigo|teal)-(700|800)/, variants: ['dark'] },
   ],
   theme: {
     extend: {
